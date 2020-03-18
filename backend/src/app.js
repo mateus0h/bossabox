8521d81c59bca98df6b3847ae47ef01710a47f9a
Mateus <mateus3u@gmail.com>
2020-03-18 09:57:29 -0300

    crud de usuarios, session e adc midlewares de validacao

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,20 @@
 import 'dotenv/config';
 
 import express from 'express';
+
 import routes from './routes';
+
+import './database';
 
 class App {
   constructor() {
     this.server = express();
-    this.midllewares();
+
+    this.middlewares();
     this.routes();
   }
 
-  midllewares() {
+  middlewares() {
     this.server.use(express.json());
   }
 
---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,48 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ erro: 'User already exists.' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId); // usuario que estou editando
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    await user.update(req.body);
+
+    const { id, name } = await User.findByPk(req.userId);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
